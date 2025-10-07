909c871320029502564100e147c55ac15d7a4fb0
Tushar Pathare <patharetushar10@gmail.com>
2025-10-08 00:35:28 +0530

    Updated Feed page and added RecentMessage component

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { dummyPostsData } from "../assets/assets";
+import { assets, dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import Stories from "../components/Stories";
 import PostCard from "../components/PostCard";
+import RecentMessage from "../components/RecentMessage";
 
 const feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -23,20 +24,24 @@ const feed = () => {
       <div>
         <Stories />
         <div className="p-4 space-y-6">
-          {
-            feeds.map((post)=>(
-              <PostCard key={post._id} post={post}/>
-            ))
-          }
+          {feeds.map((post) => (
+            <PostCard key={post._id} post={post} />
+          ))}
         </div>
       </div>
 
       {/* Right Sidebar  */}
-      <div>
-        <div>
-          <h1>Sponserd</h1>
+      <div className="max-xl:hidden sticky top-0">
+        <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+          <h3 className="text-slate-800 font-semibold">Sponserd</h3>
+          <img src={assets.sponsored_img} className="w-75 h-50 rounded-md" />
+          <p className="text-slate-600">Email marketing</p>
+          <p className="text-slate-400">
+            Supercharge your marketing with a powerful, easy- to-use platform
+            built for results.
+          </p>
         </div>
-        <h1>recent messages</h1>
+        <RecentMessage/>
       </div>
     </div>
   ) : (
