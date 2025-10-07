5f94d57ac7217825069e53995712035c8d6195c9
Tushar Pathare <patharetushar10@gmail.com>
2025-10-07 23:18:50 +0530

    Updated Feed page, CSS, and added PostCard component

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { dummyPostsData } from "../assets/assets";
 import Loading from "../components/Loading";
 import Stories from "../components/Stories";
+import PostCard from "../components/PostCard";
 
 const feed = () => {
   const [feeds, setFeeds] = useState([]);
@@ -21,7 +22,13 @@ const feed = () => {
       {/* Stories and post list */}
       <div>
         <Stories />
-        <div className="p-4 space-y-6">List of post</div>
+        <div className="p-4 space-y-6">
+          {
+            feeds.map((post)=>(
+              <PostCard key={post._id} post={post}/>
+            ))
+          }
+        </div>
       </div>
 
       {/* Right Sidebar  */}
