1d2d3c5b58f80e8423b802a71ad2a6dadaee9ef0
Tushar Pathare <patharetushar10@gmail.com>
2025-10-17 19:35:33 +0530

    Updated ProfileModel component

--- frontend/src/components/ProfileModel.jsx
+++ frontend/src/components/ProfileModel.jsx
@@ -3,7 +3,7 @@ import { dummyUserData } from "../assets/assets";
 import { Pencil, PencilIcon } from "lucide-react";
 import { useState } from "react";
 
-const ProfileModel = () => {
+const ProfileModel = ({ setShowEdit }) => {
   const user = dummyUserData;
 
   const [editForm, setEditForm] = useState({
@@ -98,6 +98,96 @@ const ProfileModel = () => {
                 </div>
               </label>
             </div>
+
+            <div>
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Name
+              </label>
+              <input
+                type="text"
+                id=""
+                className="w-full p-3 border border-gray-200 rounded-lg"
+                placeholder="Please enter your full name"
+                onChange={(e) =>
+                  setEditForm({ ...editForm, full_name: e.target.value })
+                }
+                value={editForm.full_name}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Username
+              </label>
+              <input
+                type="text"
+                id=""
+                className="w-full p-3 border border-gray-200 rounded-lg"
+                placeholder="Please enter a username"
+                onChange={(e) =>
+                  setEditForm({ ...editForm, username: e.target.value })
+                }
+                value={editForm.username}
+              />
+            </div>
+            <div>
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Bio
+              </label>
+              <textarea
+                rows={3}
+                className="w-full p-3 border border-gray-200 rounded-lg"
+                placeholder="Please enter a short bio"
+                onChange={(e) =>
+                  setEditForm({ ...editForm, bio: e.target.value })
+                }
+                value={editForm.bio}
+              />
+            </div>
+
+            <div>
+              <label
+                htmlFor=""
+                className="block text-sm font-medium text-gray-700 mb-1"
+              >
+                Loaction
+              </label>
+              <input
+                type="text"
+                id=""
+                className="w-full p-3 border border-gray-200 rounded-lg"
+                placeholder="Please enter your location"
+                onChange={(e) =>
+                  setEditForm({ ...editForm, location: e.target.value })
+                }
+                value={editForm.location}
+              />
+            </div>
+
+            <div className="flex justify-end space-x-3 pt-6">
+              <button
+                type="button"
+                onClick={() => setShowEdit(false)}
+                className="px-4 py-2 border border-gray-300 rounded-lg text-gray-700 hover:bg-hover-50 transition-colors cursor-pointer"
+              >
+                Cancel
+              </button>
+              <button
+                type="submit"
+                className="px-6 py-2 bg-gradient-to-r from-indigo-500 to-purple-600 text-white rounded-lg hover:from-indigo-600 hover:to-purple-700 transition cursor-pointer"
+              >
+                Save Chnages
+              </button>
+            </div>
+            
           </form>
         </div>
       </div>
